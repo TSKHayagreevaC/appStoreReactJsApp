8fdaf8e7ccba5950e2c7f8746b093166f13e209f
root <srikanth.thiru12@gmail.com>
2021-12-13 12:57:12 +0000

    commit with decent functionality and features

---
+++ src/components/AppItem/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const AppItem = props => {
+  const {eachAppItem} = props
+  const {appName, imageUrl} = eachAppItem
+  return (
+    <li className="app-item-container">
+      <img className="app-item-image" src={imageUrl} alt="itemImage" />
+      <p className="app-item-description">{appName}</p>
+    </li>
+  )
+}
+
+export default AppItem
